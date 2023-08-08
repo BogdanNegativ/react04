c75a010da8d4e228d43fb6e95d82b22ca8aa1122
Bohdan Liashuk <luashuk2002@gmail.com>
2023-08-08 21:06:39 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Counter from './components/Counter';
+import Counter2 from './components/Counter2';
+import StopWatch from './components/StopWatch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = { isVisible: true, theme: 'light' }
+
+  setVisible = () => {
+    this.setState({ isVisible: !this.state.isVisible })
+  }
+  changeTheme = () => {
+    this.setState(
+      { theme: this.state.theme === 'light' ? 'dark' : 'light' }
+    )
+  }
+  render() {
+    return (
+      <div>
+        {/* <button onClick={this.setVisible}>visible</button>
+        <button onClick={this.changeTheme}>change theme</button>
+        {this.state.isVisible && <Counter theme={this.state.theme} />}
+        {this.state.isVisible && <Counter2 theme={this.state.theme} />} */}
+
+        <StopWatch/>
+      </div >
+    );
+  }
 }
 
 export default App;
